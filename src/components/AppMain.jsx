0be4490d5635b18d8c82172551bbfaa2341d2085
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-12 15:54:09 +0100

    add main component
    add css style

---
+++ src/components/AppMain.jsx
@@ -0,0 +1,25 @@
+import image from '../assets/img/procione.jpg'
+export default function AppMain() {
+
+    return (
+
+        <main>
+            <div className="container">
+                <div className="card">
+                    <img src={image} alt="" />
+                    <div className="text-section">
+                        <div className="title">
+                            <strong>Lui è Jack!</strong>
+                        </div>
+                        <div className="description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut at non error molestias reiciendis obcaecati consequatur maiores ipsum! Ullam corporis aut suscipit quas maxime ad perspiciatis earum cumque sint quaerat.
+                        </div>
+                        <button>Approfondisci Jack</button>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+    )
+
+}
